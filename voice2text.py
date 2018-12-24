7d2ace45e584276a901149bdc8b72624629caf4e
ZsgsDesign <zsgsdesign@gmail.com>
2018-12-25 00:06:14 +0800

    silghtly improve

--- voice2text.py
+++ voice2text.py
@@ -5,17 +5,8 @@ from subscription_key import speech_key
 assert speech_key
 
 audio_config = speechsdk.audio.AudioConfig(use_default_microphone=False,filename="freetalks/week 1/Q17010217.wav")
-
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region="eastasia")
-
 speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config,audio_config=audio_config)
-
-# print("Say something...")
-
-# Performs recognition. recognize_once() returns when the first utterance has been recognized,
-# so it is suitable only for single shot recognition like command or query. For long-running
-# recognition, use start_continuous_recognition() instead, or if you want to run recognition in a
-# non-blocking manner, use recognize_once_async().
 
 proc=True
 dots=1
@@ -45,9 +36,12 @@ def session_stopped_func(e):
     speech_recognizer.stop_continuous_recognition()
     print("[Finished] " + ans)
 
+def failed(e):
+    print("[Canceled]")
+
 speech_recognizer.recognized.connect(recoged)
 speech_recognizer.recognizing.connect(recog)
-# speech_recognizer.canceled.connect(recog)
+speech_recognizer.canceled.connect(failed)
 speech_recognizer.session_started.connect(session_started_func)
 speech_recognizer.session_stopped.connect(session_stopped_func)
 
@@ -56,17 +50,3 @@ speech_recognizer.start_continuous_recognition()
 while proc:
     time.sleep(1)
     continue
-
-# ret=speechsdk.SpeechRecognitionResult.text.getter(ret)
-
-# print(result)
-
-# if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-#     print("Recognized: {}".format(result.text))
-# elif result.reason == speechsdk.ResultReason.NoMatch:
-#     print("No speech could be recognized: {}".format(result.no_match_details))
-# elif result.reason == speechsdk.ResultReason.Canceled:
-#     cancellation_details = result.cancellation_details
-#     print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-#     if cancellation_details.reason == speechsdk.CancellationReason.Error:
-#         print("Error details: {}".format(cancellation_details.error_details))
